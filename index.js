78a997919a882e078ce7f87718883c4a0542d92b
shabnaz21 <shabnaz608@gmail.com>
2023-10-20 19:05:28 +0600

    read product cart data

--- index.js
+++ index.js
@@ -99,6 +99,12 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/carts', async (req, res) => {
+            const cursor = cartsCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
         
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
